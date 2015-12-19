8170e56240bdf5e93a54d6cceb16f77a784cbe4d
kenoung <e0003273@u.nus.edu>
2015-12-20 01:49:46 +0800

    full dir path required for MYSQL definition

--- src/config/config.php
+++ src/config/config.php
@@ -4,6 +4,6 @@
 date_default_timezone_set('Asia/Singapore');
 
 // MySQL connection script
-define('MySQL', 'mysql_connect.php');
+define('MYSQL', 'C:\\xampp\htdocs\myblog1\src\config\mysql_connect.php');
 
 ?>
