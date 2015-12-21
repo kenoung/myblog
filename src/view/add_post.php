d9a2f9d9420a3cab5d79eccf5f9a574126304676
kenoung <e0003273@u.nus.edu>
2015-12-21 17:28:46 +0800

    ck editor

--- src/view/add_post.php
+++ src/view/add_post.php
@@ -63,7 +63,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Handle the form
 			<div class="well form-group blog-post">
 				<p><h3>Title</h3> <input type="text" name="title" class="form-control" required autofocus></p>
 				<p><h3>Post</h3> <textarea name="post" class="form-control" rows="10"></textarea></p>
-
 				<hr>
 				<p class="text-center"><button type="submit" class="btn btn-success" name="submit">Post!</button></p>
 
