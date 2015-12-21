a1a72967076488449bc61b4bfe30a51ef9e6db16
kenoung <e0003273@u.nus.edu>
2015-12-21 16:47:06 +0800

    tinymce editor,cannot control padding

--- src/view/delete_post.php
+++ src/view/delete_post.php
@@ -37,7 +37,7 @@ if (isset($_GET['post_id']) && is_numeric($_GET['post_id']) && ($_GET['post_id']
 		print '<form role="form" action="delete_post.php" method="post">
 			<div class="well form-group blog-post">
 				<p><h3>Title</h3> <input type="text" name="title" class="form-control" value="' . htmlentities($row['title']) . '"></p>
-				<p><h3>Post</h3> <textarea name="post" class="form-control" rows="10">' . htmlentities($row['post']) . '</textarea></p>
+				<p><h3>Post</h3> <textarea name="post" class="form-control" rows="10">' . base64_decode($row['post']) . '</textarea></p>
 				<hr><p class="text-center"><button type="submit" class="btn btn-danger" name="submit">DELETE!</button></p>
 				<input type="hidden" name="post_id" value="' . $_GET['post_id'] . '" />
 		</form>';
